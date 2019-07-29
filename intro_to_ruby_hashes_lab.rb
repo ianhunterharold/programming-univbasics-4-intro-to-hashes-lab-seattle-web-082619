c64ee36a39b632704d481e09853e95e12a9504f9
Ian Harold <ianhunterharold@gmail.com>
2019-07-29 18:11:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,6 +37,7 @@ def update_counting_hash(hash, key)
     hash[key]+= 1
   else 
     hash[key] = 1 
+  end 
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
@@ -44,8 +45,8 @@ end
 
 # referenc code below 
 
-if shipping_manifest["top hat"]
-  shipping_manifest["top hat"] += 1
-else
-  puts "Key not found!"
-end
+# if shipping_manifest["top hat"]
+#   shipping_manifest["top hat"] += 1
+# else
+#   puts "Key not found!"
+# end
