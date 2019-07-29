0b7d060aa2c34974f7f647c30a60ec99d79ba088
Ian Harold <ianhunterharold@gmail.com>
2019-07-29 17:51:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,7 @@ end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
-  hash = {:key => "value"}
+  hash = {key => value}
   if (hash[:key] === "value")
   return hash[:key]
 else 
