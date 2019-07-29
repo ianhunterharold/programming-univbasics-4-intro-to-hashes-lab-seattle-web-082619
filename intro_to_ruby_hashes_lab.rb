232a3ca28bff6e6977d8fc1716500da6ef751b74
Ian Harold <ianhunterharold@gmail.com>
2019-07-29 17:55:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,14 +29,8 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
- 
   hash = {key => value}
-  
- 
-else 
-  return nil 
-end 
+  return hash[key]
 end
 
 def update_counting_hash(hash, key)
